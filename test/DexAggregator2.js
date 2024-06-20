f55aa3ee447b3eb27edd4d46fd920ae70b16c34c
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-06-19 17:36:20 -0700

    clean up

--- test/DexAggregator2.js
+++ test/DexAggregator2.js
@@ -10,36 +10,19 @@ const formatEther = (n) => {
 };
 
 describe("Dex Aggregator2", () => {
-  let dexAggregator,
-  uniswapRouter,
-  sushiswapRouter,
-    accounts,
-    swaper
+  let dexAggregator, uniswapRouter, sushiswapRouter, accounts, swaper;
 
   beforeEach(async () => {
-    uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-    sushiswapRouter = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
+    uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    sushiswapRouter = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
     accounts = await ethers.getSigners();
     deployer = accounts[0];
 
     swaper = accounts[1];
 
-
     const DEX_AGGREGATOR = await ethers.getContractFactory("DexAggregator2");
     dexAggregator = await DEX_AGGREGATOR.deploy(uniswapRouter, sushiswapRouter);
 
-//     // first DEX_AGGREGATOR function call
-//     amount = tokens(4);
-//     amm2Token2ReturnAmount = await amm2.calculateTokenSwap(
-//       token1.address,
-//       token2.address,
-//       amount
-//     );
-//     amm1Token1ReturnAmount = await amm1.calculateTokenSwap(
-//       token2.address,
-//       token1.address,
-//       amount
-//     );
   });
   describe("Deployment", () => {
     it("has an address", async () => {
