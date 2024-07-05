0e3c631202caaaf2d5f24d3536c8832507821143
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-07-05 13:39:35 -0700

    updated navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,10 +14,6 @@ const Navigation = () => {
   // const amm = useSelector((state) => state.amm.contract);
   const dispatch = useDispatch();
 
-  const handleConnect = async () => {
-    const account = await loadAccount(dispatch);
-  };
-
   const handleNetwork = async (e) => {
     console.log(e.target.value, chainId);
     await window.ethereum.request({
@@ -27,8 +23,8 @@ const Navigation = () => {
   };
 
   return (
-      <Navbar className="my-3 bg-dark" expand="lg">
-        {/* <img
+    <Navbar className="my-3 bg-dark" expand="lg">
+      {/* <img
           alt="logo"
           src={logo}
           width="50"
@@ -36,52 +32,45 @@ const Navigation = () => {
           style={{ borderRadius: '100%'}}
           className="d-inline-block align-top mx-3"
         /> */}
-        <Navbar.Brand className="text-light fw-bold" href="#">
-          Dex Aggregator
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="nav" />
-        <Navbar.Collapse id="nav" className="justify-content-end">
-          <div className="d-flex justify-content-end mt-3 bg-dark border-light">
-            <Form.Select
+      <Navbar.Brand className="text-light fw-bold" href="#">
+        Dex Aggregator
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="nav" />
+      <Navbar.Collapse id="nav" className="justify-content-end">
+        <div className="d-flex justify-content-end mt-3 bg-dark border-light">
+          {account ? (
+            <Navbar.Text style={{ marginRight: '20px'}} className="d-flex align-items-center text-light">
+              {account.slice(0, 5) + "..." + account.slice(38, 42)}
+              <Blockies
+                seed={account}
+                size={10}
+                scale={3}
+                color="purple"
+                bgColor="blue"
+                spotColor="white"
+                className="identicon mx-2"
+              />
+            </Navbar.Text>
+          ) : (
+            <></>
+          )}
+          <Form.Select
             className="bg-light border-light text-dark"
-              aria-label="Network Selector"
-              value={config[chainId] ? `0x${chainId.toString(16)}` : `0`}
-              onChange={handleNetwork}
-              style={{ maxWidth: "200px", marginRight: "20px" }}
-            >
-              <option value="0" disabled>
-                Select Network
-              </option>
-              <option value="0x7A69">Localhost</option>
-              <option value="0xaa36a7">Sepolia</option>
-            </Form.Select>
-
-            {account ? (
-              <Navbar.Text className="d-flex align-items-center text-light">
-                {account.slice(0, 5) + "..." + account.slice(38, 42)}
-                <Blockies
-                  seed={account}
-                  size={10}
-                  scale={3}
-                  color="purple"
-                  bgColor="blue"
-                  spotColor="white"
-                  className="identicon mx-2"
-                />
-              </Navbar.Text>
-            ) : (
-              <Button
-                className="bg-light border-light text-dark"
-                onClick={handleConnect}
-              >
-                Connect
-              </Button>
-            )}
-          </div>
-        </Navbar.Collapse>
-      </Navbar>
+            aria-label="Network Selector"
+            value={config[chainId] ? `0x${chainId.toString(16)}` : `0`}
+            onChange={handleNetwork}
+            style={{ maxWidth: "200px", marginRight: "20px" }}
+          >
+            <option value="0" disabled>
+              Select Network
+            </option>
+            <option value="0x7A69">Localhost</option>
+            <option value="0xaa36a7">Sepolia</option>
+          </Form.Select>
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
 export default Navigation;
-
