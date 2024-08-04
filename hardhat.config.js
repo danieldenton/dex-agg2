1aeb9ed7d3f659b055cd747f74bd8b10fde29612
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-03 17:20:16 -0700

    forking the arbitrum mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,17 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 const privateKey = process.env.PRIVATE_KEY || "";
 
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {},
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [privateKey],
-    },
-  },
+    hardhat: {
+      forking: {
+        chainId: 31337,
+        url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 223528000
+      },
+    }
+  }
 };
