ea12de1778ff8f1bebe4868fa321683df791940e
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-08-06 17:58:20 -0700

    updated dexSelector. Its looking good

--- contracts/DexAggregator.sol
+++ contracts/DexAggregator.sol
@@ -2,8 +2,8 @@
 pragma solidity =0.7.6;
 pragma abicoder v2;
 
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
 contract DexAggregator {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
@@ -12,13 +12,11 @@ contract DexAggregator {
     uint24 public constant feeTier = 3000;
     address public owner;
 
-
     constructor(address[] memory _routers, uint16 _defaultSlippagePercent) {
         routers = _routers;
         owner = msg.sender;
         defaultSlippagePercent = _defaultSlippagePercent;
     }
-
 
     function separateFee(
         uint256 _amount
@@ -27,41 +25,44 @@ contract DexAggregator {
         amountAfterFee = _amount - fee;
     }
 
-// path is going to be 2 token addresses in an array
-// router is the dex address
+    // path is going to be 2 token addresses in an array
+    // router is the dex address
     function dexSelector(
         address[] memory _path,
         uint256 _amount
-    ) public view returns (address chosenDex, uint256 returnAmount) {
+    ) public view returns (address chosenDex, uint256 highestReturnAmount) {
         (uint256 _amountAfterFee, ) = separateFee(_amount);
 
-        returnAmount = swapRouter()
-
-        for (i = 0; i < routers.length; i++) {
-            amountOut = ISwapRouter(routers[i]).getAmountsOut(
-                    _amount,
-                    _path
-                );
-            if (amountOut[lastHop] > bestAmountOut) {
-                bestAmountOut = amountOut[lastHop];
-                bestRouter = routers[i];
+        for (uint256 i = 0; i < routers.length; i++) {
+            uint256 returnAmount = ISwapRouter(routers[i]).getAmountsOut(
+                _amountAfterFee,
+                _path
+            );
+            if (returnAmount > highestReturnAmount) {
+                highestReturnAmount = returnAmount;
+                chosenDex = routers[i];
             }
         }
-
     }
 
-    function swapWETHForDAI(uint256 amountIn) external returns (uint256 amountOut) {
-
+    function swapWETHForDAI(
+        uint256 amountIn
+    ) external returns (uint256 amountOut) {
         // Transfer the specified amount of WETH9 to this contract.
-        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
+        TransferHelper.safeTransferFrom(
+            WETH9,
+            msg.sender,
+            address(this),
+            amountIn
+        );
         // Approve the router to spend WETH9.
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
         // Note: To use this example, you should explicitly set slippage limits, omitting for simplicity
         uint256 minOut = /* Calculate min output */ 0;
         uint160 priceLimit = /* Calculate price limit */ 0;
         // Create the params that will be used to execute the swap
-        ISwapRouter.ExactInputSingleParams memory params =
-            ISwapRouter.ExactInputSingleParams({
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
                 tokenIn: WETH9,
                 tokenOut: DAI,
                 fee: feeTier,
